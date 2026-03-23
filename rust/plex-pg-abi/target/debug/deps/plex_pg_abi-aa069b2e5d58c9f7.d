b77c26694edca3aec542d5dc72195d5093c38d48
Sander <sander@Sanders-Mac-mini.local>
2026-03-23 16:40:59 +0100

    chore: ignore generated ABI build artifacts

--- rust/plex-pg-abi/target/debug/deps/plex_pg_abi-aa069b2e5d58c9f7.d
+++
@@ -1,5 +0,0 @@
-/Users/sander/plex-postgresql-develop/rust/plex-pg-abi/target/debug/deps/plex_pg_abi-aa069b2e5d58c9f7.d: src/lib.rs
-
-/Users/sander/plex-postgresql-develop/rust/plex-pg-abi/target/debug/deps/libplex_pg_abi-aa069b2e5d58c9f7.rmeta: src/lib.rs
-
-src/lib.rs:
